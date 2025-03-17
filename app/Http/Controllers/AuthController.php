b9bd66ec4ad1276ab549148ecc004c42ab259f37
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-03-17 09:06:06 -0300

    Init project, creating authentication APIs (Login, Sign-Up and Get User Info)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class AuthController extends Controller
+{
+    public function registerUser(RegisterUserRequest $request)
+    {
+        $body = $request->only('fullName', 'email', 'password');
+        $file = $request->file('profileImageUrl');
+        
+        $path = Storage::disk('media')->put('', $file);
+
+        $user = User::create([
+            'full_name' => $body['fullName'],
+            'email' => $body['email'],
+            'password' => $body['password'],
+        ]);
+
+        $user->addMedia(public_path('media/' . $path))
+            ->toMediaCollection();
+
+        return $user;
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Login ou senha estão incorretos!'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        $user = Auth::user();
+        $user->profile_image_url = $user->getFirstMediaUrl();
+        return response()->json($user);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'registerUser']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->group(function() {
+        Route::get('me', [AuthController::class, 'me']);
+    });
+});
