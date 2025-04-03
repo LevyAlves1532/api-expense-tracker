aac7077a788ad7cccff31095ae98d5c09e1ab9ab
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-04-03 08:36:19 -0300

    Adjustment data from API Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Expense;
 use App\Models\Income;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -22,8 +21,8 @@ class HomeController extends Controller
             ->where('date', '>', $last60Days)
             ->orderBy('date', 'desc')
             ->get();
-        $data['last_60_days_income_transactions'] = $last60DaysIncomeTransactions;
-        $data['income_last_60_days'] = $last60DaysIncomeTransactions->sum('amount');
+        $data['last_60_days_income']['total'] = $last60DaysIncomeTransactions->sum('amount');
+        $data['last_60_days_income']['transactions'] = $last60DaysIncomeTransactions;
 
         $last30Days = Carbon::now()->subDays(60)->format('Y-m-d');
 
@@ -31,8 +30,8 @@ class HomeController extends Controller
             ->where('date', '>', $last30Days)
             ->orderBy('date', 'desc')
             ->get();
-        $data['last_30_days_expense_transactions'] = $last30DaysExpenseTransactions;
-        $data['expense_last_30_days'] = $last30DaysExpenseTransactions->sum('amount');
+        $data['last_30_days_expense']['total'] = $last30DaysExpenseTransactions->sum('amount');
+        $data['last_30_days_expense']['transactions'] = $last30DaysExpenseTransactions;
 
         $data['last_transactions'] = collect([]);
         $data['last_transactions'] = $data['last_transactions']->merge(Income::where('user_id', Auth::id())
@@ -51,9 +50,11 @@ class HomeController extends Controller
                 $income['type'] = 'expense';
                 return $income;
             }));
-        $data['last_transactions'] = $data['last_transactions']->sortByDesc(function ($transaction) {
-            return $transaction->date;
-        });
+        $data['last_transactions'] = [
+            ...$data['last_transactions']->sortByDesc(function ($transaction) {
+                return $transaction->date;
+            })->toArray(),
+        ];
 
         return $data;
     }
